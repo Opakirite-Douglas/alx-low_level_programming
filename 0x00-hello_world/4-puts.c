aeb8b91edec97fdeaeb668961cd8a16deb4f6892
Opakirite-Douglas <ddebbyd82@gmail.com>
2022-12-09 08:52:07 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - my first line of c programming _ print a new line of code using puts function
+ *
+ * Return: 0
+ */
+
+int main (void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	Return (0);
+}
