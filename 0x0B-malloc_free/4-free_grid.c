1fcc0ee20d7892702dde57b4543655cf2b39a1f0
Opakirite-Douglas <ddebbyd82@gmail.com>
2023-01-10 14:24:51 -0800

    project on C - malloc, free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -38,19 +38,19 @@ h++;
 
 int main(void)
 {
-	int **grid;
+int **grid;
 
-	grid = alloc_grid(6, 4);
-	if (grid == NULL)
-	{
-		return (1);
-	}
+grid = alloc_grid(6, 4);
+if (grid == NULL)
+{
+return (1);
+}
 
-	 print_grid(grid, 6, 4);
-	 printf("\n");
-	 grid[0][3] = 98;
-	 grid[3][4] = 402;
-	 print_grid(grid, 6, 4);
-	 free_grid(grid, 4);
-	 			return (0);
+print_grid(grid, 6, 4);
+printf("\n");
+grid[0][3] = 98;
+grid[3][4] = 402;
+print_grid(grid, 6, 4);
+free_grid(grid, 4);
+return (0);
 }
