79be89f989100d9d34cf555666a0ba5457061af8
Opakirite-Douglas <ddebbyd82@gmail.com>
2023-01-10 14:39:36 -0800

    corrected function

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,56 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * print_grid - prints a grid of integers
- * @grid: the address of the two dimensional grid
- * @width: width of the grid
+ * free_grid - frees a 2 dimensional grid
+ * @grid: the grid
  * @height: height of the grid
- *
- * Return: Nothing.
  */
 
-void print_grid(int **grid, int width, int height)
+void free_grid(int **grid, int height)
 {
-int w;
-int h;
+	int i;
 
-h = 0;
-while (h < height)
-{
-w = 0;
-while (w < width)
-{
-printf("%d ", grid[h][w]);
-w++;
-}
-printf("\n");
-h++;
-}
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid)
 }
 
-/**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
 
-int main(void)
-{
-int **grid;
-
-grid = alloc_grid(6, 4);
-if (grid == NULL)
-{
-return (1);
-}
-
-print_grid(grid, 6, 4);
-printf("\n");
-grid[0][3] = 98;
-grid[3][4] = 402;
-print_grid(grid, 6, 4);
-free_grid(grid, 4);
-return (0);
-}
