0e4c564d95336f77269bc2ec9ae29b668e95d38c
Opakirite-Douglas <117810253+Opakirite-Douglas@users.noreply.github.com>
2023-11-21 11:02:03 +0100

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,17 +1,12 @@
-/*
- * File: 1-listint_len.c
- * Author: Onawole A bayomi Robert
- */
-
 #include "lists.h"
 #include <stdio.h>
 
 /**
- * listint_len - Returns the number of elements in a linked listint_t list.
+ * Returns the number of elements in a linked listint_t list.
  * @h: A pointer to the head of the listint_t list.
- *
  * Return: The number of elements in the listint_t list.
  */
+
 size_t listint_len(const listint_t *h)
 {
 	size_t nodes = 0;
