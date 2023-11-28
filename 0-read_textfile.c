61e5fe99c71607fcdef761de266281c63dcdb47a
Opakirite-Douglas <ddebbyd82@gmail.com>
2023-11-28 02:13:22 -0800

    0-read_textfile.c

---
+++ 0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * read_textfile -This func will simply just read a text file
+ * then print it to POSIX stdout
+ * @letters: This represents the number of printed letters
+ * @filename: This is just a pointer
+ * Return: 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int opas_fd;
+	char *Bens_buff;
+	ssize_t nrd, nwr;
+
+	if (!filename)
+	{
+		return (0);
+	}
+	opas_fd = open(filename, O_RDONLY);
+
+	if (opas_fd == -1)
+	{
+		return (0);
+	}
+	opas_buff = malloc(sizeof(char) * letters);
+	if (opas_buff == NULL)
+	{
+		return (0);
+	}
+
+	nrd = read(opas_fd, opas_buff, letters);
+	nwr = write(STDOUT_FILENO, opas_buff, nrd);
+
+	close(opas_fd);
+	free(opas_buff);
+
+	return (nwr);
+}
